94058842fc3bd43ea47c4e1a715de0cf715889d7
moritz155 <m.schelten@gmx.de>
2021-05-15 12:50:41 +0200

    fix merge conflict

--- Database_communication/safeBrowsing/flask_playground.py
+++ Database_communication/safeBrowsing/flask_playground.py
@@ -3,11 +3,10 @@ import database_playground
 from flask_cors import CORS  # import with me with the following cmd: pip install flask-cors --upgrade
 import re
 
-from safeBrowsing.interpret_whotracksme import generic_sql_query
+from safeBrowsing.interpret_whotracksme import generic_sql_query, calc_label
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/')
 def hello_world():
@@ -41,7 +40,7 @@ def receive_urls():
 @app.route('/urls/', methods=['GET'])
 def urls():
     query = "SELECT site, category FROM sites_data ORDER BY site ASC"
-    return generic_sql_query(query)
+    return jsonify(generic_sql_query(query))
 
 
 #  return render_template("list.html", rows=rows)
@@ -50,13 +49,13 @@ def urls():
 @app.route('/urls/<url>', methods=['GET'])
 def url(url):
     query = f"SELECT site, category, cookies, requests_tracking FROM sites_data WHERE site = \"{url}\" ORDER BY site ASC"
-    return generic_sql_query(query)
+    return jsonify(generic_sql_query(query))
 
 
 @app.route('/tracker/<url>', methods=['GET']) #
 def trackers_category_from_url(url):
     query = f"  SELECT categories.name, sites_trackers_data.site AS has_this_tracker,trackers.name, trackers.website_url FROM trackers, categories, sites_trackers_data WHERE trackers.category_id = categories.id AND trackers.id = sites_trackers_data.tracker  AND sites_trackers_data.site =\"{url}\""
-    return generic_sql_query(query)
+    return jsonify(generic_sql_query(query))
 
 
 #  return render_template("list.html", rows=rows)
--- Database_communication/safeBrowsing/interpret_whotracksme.py
+++ Database_communication/safeBrowsing/interpret_whotracksme.py
@@ -1,5 +1,4 @@
 from flask import jsonify
-
 from safeBrowsing import database_playground
 
 
@@ -9,4 +8,22 @@ def generic_sql_query(query):
 
     con.execute(query)
     rows = con.fetchall()
-    return jsonify(rows)
+    return rows
+
+"""idea: Each time we find out some bad information about a domain this domain gets points(ofc relatively to the 
+information we found out) At the end we check the score of each domain and give the domains a fitting label """
+
+
+def calc_label(domain_list):
+    domain_dict = {}
+    for domain in domain_list:
+        domain_dict[domain] = whotracksme_score(domain)
+
+
+def whotracksme_score(domain):
+    query = f"  SELECT categories.name, sites_trackers_data.site AS has_this_tracker,trackers.name, trackers.website_url FROM trackers, categories, sites_trackers_data WHERE trackers.category_id = categories.id AND trackers.id = sites_trackers_data.tracker  AND sites_trackers_data.site =\"{domain}\""
+    trackers = generic_sql_query(query)
+    print(trackers)
+    for cookie in trackers:
+        print(cookie)
+    return 0
