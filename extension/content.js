cf7fddd0349e504398eed97faa21659bad6153cd
moritz155 <m.schelten@gmx.de>
2021-05-17 00:33:37 +0200

    I told you so

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
-var result = $('h3').closest('div')
+var result = $('.LC20lb').closest('div')
 var img = $('<img class="code-selector">');
 img.attr('src', "https://lisztos.s3.amazonaws.com/images/icon_128_clear.png");
 //img.attr('src', 'https://www.google.com/search?q=orange&sxsrf=ALeKk02SZqG-A6h6nM_XrHyIU98gIgRk9w:1621172387851&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjDl7Diqc7wAhXTCWMBHY61BmsQ_AUoA3oECAIQBQ&biw=1920&bih=937#imgrc=kBUCtsWcfmgoVM'); //select img from src folder? 
@@ -8,15 +8,17 @@ img.css('margin-left', '-2.25em')
 img.css('cursor', 'pointer')
 img.css('margin-top', '0.5em')
 img.appendTo(result);
-//var all_labels = ["https://lisztos.s3.amazonaws.com/images/icon_128_clear.png","",""]
+console.log(result);
+var all_labels = ["https://lisztos.s3.amazonaws.com/images/icon_128_clear.png","https://www.gettyimages.de/gi-resources/images/500px/983794168.jpg","https://www.gettyimages.de/gi-resources/images/500px/983794168.jpg"]
 $('.code-selector').on('click', function(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      answer = JSON.parse(this.responseText)
-      console.log(answer);
-      console.log(answer[0]);
-      findLabels(answer)
+      console.log(this.responseText)
+      var output = JSON.parse(JSON.parse(this.responseText)); // dont know why but you have to parse it twice
+      for (var key of Object.keys(output)) {
+        findLabel(key, output[key]);
+      }
     }
   };
 
@@ -28,17 +30,12 @@ $('.code-selector').on('click', function(){
 
   /*GET method for key-value list (domain, [1,2,3])
   set img depending on number
-  
-  
-  
-  
-  
-  
+
   */
 })
 function printLabel(whereTo, img_index){
   var img = $('<img class="code-selector">');
-  img.attr('src', all_labels[img_index]);
+  img.attr('src', all_labels[img_index - 1]);
   img.css('width', '25px')
   img.css('float', 'left')
   img.css('margin-left', '-2.25em')
@@ -46,23 +43,21 @@ function printLabel(whereTo, img_index){
   img.css('margin-top', '0.5em')
   img.appendTo(whereTo);
 }
-function findLabels(answer){
-  for( var dict of answer){
-    var div = findByDomain(dict[0]);
+function findLabel(domain, label_index){
+    var div = findByDomain(domain);
     if(div === null){ // should not happen as it means that the url was not found in website
-      break;
+      return;
     }
-    printLabel(div, dict[1]);
-
-  }
+    printLabel(div, label_index);
 }
+
 function findByDomain(domain){
   var elems = document.getElementsByClassName("yuRUbf");
   var urls = "";
   for(var div of elems){
-    var url = i.children[0].href
+    var url = div.children[0].href
     if(url.includes(domain)){
-      return i
+      return div
     }
   }
   return null;
