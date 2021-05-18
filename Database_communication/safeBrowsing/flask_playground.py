dc61920723bcc43960ff902d044c36efc5dd056b
freefreeP <66095628+freefreeP@users.noreply.github.com>
2021-05-18 15:33:06 +0200

    top 50 website mit datenbank model

--- Database_communication/safeBrowsing/flask_playground.py
+++ Database_communication/safeBrowsing/flask_playground.py
@@ -1,12 +1,57 @@
+from os import name
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-import database_playground
+import database_playground 
 from flask_cors import CORS  # import with me with the following cmd: pip install flask-cors --upgrade
 import re
 import json
-from safeBrowsing.interpret_whotracksme import generic_sql_query, calc_label
+from interpret_whotracksme import generic_sql_query, calc_label
+import sqlite3
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import csv
+
+
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datenbank.db'
+db = SQLAlchemy(app)
+
+class website(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    domain = db.Column(db.String, nullable = False)
+    rating = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/datenbank') # Datenbank ausgeben
+def index():
+    new_domain = website(domain = "manjak")
+    # return redirect('/alii')
+    # #return "hat nicht geklappt"
+    db.session.add(new_domain)
+    db.session.commit()
+    #con = db.session.cursor()
+    #con.execute("SELECT * from website")
+    session = db.session()
+    cursor = session.execute("SELECT * from website order by date_created desc").cursor
+    rows = cursor.fetchall()
+    return jsonify(rows)
+
+    #return db.engine.execute('select * from website')
+
+    #return("hat geklappt")
+
+# @app.route('/alii')
+# def func():
+
+#     query = "SELECT * FROM "
+#     return jsonify(generic_sql_query(query))
+
 
 @app.route('/')
 def hello_world():
@@ -47,7 +92,7 @@ def receive_urls():
 
 @app.route('/ids/', methods=['GET'])
 def ids():
-    query = "SELECT ID, Website FROM top500 ORDER BY ID ASC"
+    query = "SELECT * FROM top500 ORDER BY ID ASC"
     return jsonify(generic_sql_query(query))
 
 @app.route('/ids/<id>', methods=['GET'])
@@ -80,4 +125,21 @@ def trackers_category_from_url(url):
 
 
 if __name__ == '__main__':
+    db.create_all()
+    ## hier werden die top 50 deutschen Websiten hinzugefügt
+    with open('top50websites.csv','r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for line in csv_reader:
+            if(line[2] != "Adult"):
+                r = (line[1].split('.')[0])
+                new_domain = website(domain = r)
+                db.session.add(new_domain)
+                db.session.commit()
+    
+    print("wurde hinzugefügt")
+
     app.run(debug=True)
+
+
+    
