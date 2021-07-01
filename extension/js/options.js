eec6a5330617849f27d50a6769fd8ac76bf71be3
Simon Kahl <simon.kahl@gmx.de>
2021-07-01 15:08:17 +0200

    edited options page

--- extension/js/options.js
+++ extension/js/options.js
@@ -13,3 +13,75 @@ window.addEventListener("load", function(event) {
     });
 
 });
+
+const PAGES_KEY = 'pages';
+
+const toPromise = (callback) => {
+    const promise = new Promise((resolve, reject) => {
+        try {
+            callback(resolve, reject);
+        }
+        catch (err) {
+            reject(err);
+        }
+    });
+    return promise;
+}
+
+
+class PageService {
+
+    static getPages = () => {
+        return toPromise((resolve, reject) => {
+            chrome.storage.local.get([PAGES_KEY], (result) => {
+                if (chrome.runtime.lastError)
+                    reject(chrome.runtime.lastError);
+
+                const researches = result.pages ?? [];
+                resolve(researches);
+            });
+        });
+    }
+
+    static savePage = async (title, url) => {
+        const pages = await this.getPages();
+        const updatedPages = [...pages, { title, url }];
+
+        return toPromise((resolve, reject) => {
+
+            chrome.storage.local.set({ [PAGES_KEY]: updatedPages }, () => {           
+                if (chrome.runtime.lastError)
+                    reject(chrome.runtime.lastError);
+                resolve(updatedPages);
+            });
+        });
+    }
+
+    static clearPages = () => {
+        return toPromise((resolve, reject) => {
+            chrome.storage.local.remove([PAGES_KEY], () => {
+                if (chrome.runtime.lastError)
+                    reject(chrome.runtime.lastError);
+                resolve();
+            });
+        });
+    }
+}
+
+
+/*
+If you're interested in tracking changes made to a data object, you can add a listener to its onChanged event. 
+Whenever anything changes in storage, that event fires. Here's sample code to listen for saved changes:
+*/
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (var key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
+  });
