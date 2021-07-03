afc84420f749e7a674ff8a3c614c5d69a0357828
moritz155 <m.schelten@gmx.de>
2021-07-03 13:22:40 +0200

    implement possibility to create new label

--- extension/privacyScore.js
+++ extension/privacyScore.js
@@ -1,16 +1,46 @@
-window.addEventListener("load" , function() {});
+window.addEventListener("load", function() {
 
-chrome.storage.local.get(function(data){
+    document.getElementById("submitButton").addEventListener("click", function() {
+        console.log("jsjs");
+        url = document.getElementById("url").value;
+        var hardcoded_preference = { "whotracksme": ["Facebook", "Amazon"], "privacyspy": [], "google_safeBrowsing": [], "phishstats": [], "webrisk": [] }
+            // call a method that looks for preferences stored in storage api
+        var hardcoded_expert_mode = false
+        var body = url + "SPLITME" + JSON.stringify(hardcoded_preference) + "SPLITME" + hardcoded_expert_mode
+        sendURL(body);
+    })
+
+});
+
+function sendURL(body) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // console.log(this.responseText)
+            var output = JSON.parse(JSON.parse(this.responseText)); // dont know why but you have to parse it twice
+            first_key = Object.keys(output)[0]
+            console.log(output[first_key][0]["label"]);
+            document.getElementById("calcLabel").style.display = "block";
+            document.getElementById("printLabel").innerText = "The label for the domain " + first_key + " is " + output[first_key][0]["label"] + ".";
+        }
+    };
+    console.log("hf")
+    xhttp.open("POST", "http://127.0.0.1:5000/sendurls/", true); //Flask projekt muss am laufen sein 
+    xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhttp.send(body);
+}
+
+chrome.storage.local.get(function(data) {
     var labels = Object.values((data));
     var goodLabels = 0; //green or gold
     var unknownLabels = 0;
 
     //console.log(labels); //UNCOMMENT TO SEE WHAT LABELS ARE SAVED
 
-    for(var i = 0; i < labels.length; i++) {
-        if (labels[i]>=7){     //TODO: Two score formulas are needed: one for expert mode and one for normal mode
+    for (var i = 0; i < labels.length; i++) {
+        if (labels[i] >= 7) { //TODO: Two score formulas are needed: one for expert mode and one for normal mode
             goodLabels++;
-        } else if(labels[i] === 0){
+        } else if (labels[i] === 0) {
             unknownLabels++;
         } else {
             continue;
@@ -18,29 +48,29 @@ chrome.storage.local.get(function(data){
     }
     var score = '';
 
-    if (labels.length===0){
+    if (labels.length === 0) {
         document.getElementById("privacyScore_number").innerHTML = '¯\\_(ツ)_/¯';
         document.getElementById("privacyScore_info").innerHTML = 'Nothing to show yet.';
     } else {
-        score = Math.round(goodLabels / (labels.length-unknownLabels) * 100);
+        score = Math.round(goodLabels / (labels.length - unknownLabels) * 100);
         document.getElementById("privacyScore_number").innerHTML = score.toString() + '%';
         document.getElementById("privacyScore_info").innerHTML = getPrivacyInfo(score);
     }
 });
 
 
-function getPrivacyInfo(score){
+function getPrivacyInfo(score) {
     var info = "";
 
     if (score >= 66) {
-        info = "Good job! You took good care about your privacy in the past week: "
-            + score + "% of your visited websites had a green label!";
+        info = "Good job! You took good care about your privacy in the past week: " +
+            score + "% of your visited websites had a green label!";
     } else if (score < 66 && score >= 33) {
-        info = "Not bad! You did quite well in the past week: "
-            + score + "% of your visited websites had a green label";
+        info = "Not bad! You did quite well in the past week: " +
+            score + "% of your visited websites had a green label";
     } else {
-        info = "Could be better! Only " + score + "% of your visited websites had a green label. "
-            + "Next time take a look at the label before you open the page."
+        info = "Could be better! Only " + score + "% of your visited websites had a green label. " +
+            "Next time take a look at the label before you open the page."
     }
     return info;
 }
@@ -48,10 +78,10 @@ function getPrivacyInfo(score){
 
 var btn = document.getElementById("reset_stats");
 
-if(btn) {
-    btn.addEventListener("click", function(){
-        chrome.storage.local.get(function(data){
-            if (Object.values(data).length===0) {
+if (btn) {
+    btn.addEventListener("click", function() {
+        chrome.storage.local.get(function(data) {
+            if (Object.values(data).length === 0) {
                 alert("Your storage is already empty");
             } else {
                 chrome.storage.local.clear();
@@ -61,6 +91,6 @@ if(btn) {
     })
 }
 
-chrome.storage.onChanged.addListener(function () {
+chrome.storage.onChanged.addListener(function() {
     location.reload();
 });
