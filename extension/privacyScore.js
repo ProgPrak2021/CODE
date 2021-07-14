97a7d71b80188407970c6a36002ea02015fd58b7
Diana Baumann <41950946+dianabaumann@users.noreply.github.com>
2021-07-14 12:15:57 +0200

    updated privacy summary view and get label can be execute by pressing the enter key

--- extension/privacyScore.js
+++ extension/privacyScore.js
@@ -1,6 +1,10 @@
 if(document.getElementById("submitButton")) {
+    document.getElementById("url").addEventListener("keyup", function () {
+        if (event.keyCode === 13) { // 13 is the enter key. TODO: Change input to form, so a submit event can be triggered.
+            document.getElementById("submitButton").click();
+        }
+    })
     document.getElementById("submitButton").addEventListener("click", function () {
-        console.log("jsjs");
         let url = document.getElementById("url").value;
         let hardcoded_preference = {
             "whotracksme": ["Facebook", "Amazon"],
@@ -11,7 +15,11 @@ if(document.getElementById("submitButton")) {
         };
         // call a method that looks for preferences stored in storage api
         let hardcoded_expert_mode = false;
-        let body = JSON.stringify({"urls": url, "preferences": JSON.stringify(hardcoded_preference), "expert":hardcoded_expert_mode})
+        let body = JSON.stringify({
+            "urls": url,
+            "preferences": JSON.stringify(hardcoded_preference),
+            "expert": hardcoded_expert_mode
+        })
         sendURL(body);
     });
 }
@@ -20,42 +28,45 @@ if(document.getElementById("submitButton")) {
 function sendURL(body) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var labels = [
+        if (this.readyState === 4 && this.status === 200) {
+            let labels = [
                 [chrome.runtime.getURL('images/not_found.png')],
                 [chrome.runtime.getURL('images/green_icon_32.png')],
                 [chrome.runtime.getURL('images/yellow_icon_32.png')],
                 [chrome.runtime.getURL('images/red_icon_32.png')]
-            ]
+            ];
             // console.log(this.responseText);
             let output = JSON.parse(JSON.parse(this.responseText)); // dont know why but you have to parse it twice
             let first_key = Object.keys(output)[0];
             document.getElementById("calcLabel").style.display = "block";
-            var img = labels[output[first_key][1]["label"]]
-            console.log(output[first_key][1])
-            document.getElementById("printLabel").innerHTML = "The label for the domain " + first_key + " is <img src= "+img+" > </img>.";
+            let img = labels[output[first_key][1]["label"]];
+            console.log(output[first_key][1]);
+            if (output[first_key][1]["label"] === 0) {
+                document.getElementById("printLabel").innerHTML = "Sorry, we don't have a label for this url yet.";
+            } else {
+                document.getElementById("printLabel").innerHTML = "The label for the domain " + first_key + " is <img src= "+img+" > </img>.";
+            }
         }
     };
-    xhttp.open("POST", "http://127.0.0.1:5000/sendurls/", true); //Flask projekt muss am laufen sein 
+    xhttp.open("POST", "http://127.0.0.1:5000/sendurls/", true); //Flask projekt muss am laufen sein
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
     xhttp.send(body);
 }
 
 chrome.storage.local.get(function(data) {
-    let domains = Object.keys(data);
-    let labels = Object.values((data));
+    let keys = Object.keys(data); //domains
+    let values = Object.values((data)); //labels
+    let all_labels = 0;
     let goodLabels = 0; //green or gold
     let unknownLabels = 0;
 
-    //console.log(domains + ': ' + labels); //UNCOMMENT TO SEE WHAT LABELS ARE SAVED
+    //console.log(keys + ': ' + values); //UNCOMMENT TO SEE WHAT LABELS ARE SAVED
 
-    for (let i = 0; i < labels.length; i++) {
-        if (labels[i] <= 3) { //both, green and gold labels are calculated
-            goodLabels++;
-        } else if (labels[i] === 0) {
-            unknownLabels++;
-        }
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] !== 'pages') {all_labels++;}
+        if (values[i] <= 3) {goodLabels++;
+        } else if (values[i] === 0) {unknownLabels++;}
     }
     let score = '';
 
@@ -63,34 +74,22 @@ chrome.storage.local.get(function(data) {
     let privacyScoreInfo = document.getElementById("privacyScore_info");
 
     if (privacyScoreNumber && privacyScoreInfo) {
-        if (labels.length === 0) {
+        if (values.length === 0) {
             privacyScoreNumber.innerHTML = '¯\\_(ツ)_/¯';
             privacyScoreInfo.innerHTML = 'Nothing to show yet.';
         } else {
-            score = Math.round(goodLabels / (labels.length-1 - unknownLabels) * 100);
+            score = Math.round(goodLabels / (values.length - unknownLabels) * 100);
             privacyScoreNumber.innerHTML = score.toString() + '%';
             privacyScoreInfo.innerHTML = getPrivacyInfo(score);
         }
     }
 
-    if (document.getElementById('recent_labels')) {
-        let listOfLabels = '';
-        let count = 0;
-        for (let i = 0; i <= domains.length-1; i++){
-            if (count < 3) {
-                listOfLabels += domains[i] + ': ' + labels[i];
-                listOfLabels += '<br>';
-                count++;
-            }
-        }
-        document.getElementById('recent_labels').innerHTML = listOfLabels;
-    }
-
     if (document.getElementById('all_labels')) {
         let listOfLabels = '';
-        for (let i = 3; i <= domains.length-1; i++){
-            listOfLabels += domains[i] + ': ' + labels[i];
-            listOfLabels += '<br>';
+        for (let i = 3; i <= keys.length-1; i++){
+            if(keys[i] !== 'pages'){
+                listOfLabels += keys[i] + ': ' + values[i] + '<br>';
+            }
         }
         document.getElementById('all_labels').innerHTML = listOfLabels;
     }
