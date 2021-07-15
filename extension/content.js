b276b3114dbb7ddb28f517506bac9eb033b8f0c1
Diana Baumann <41950946+dianabaumann@users.noreply.github.com>
2021-07-15 16:58:55 +0200

    add content.js to home.html for 'get label' form

--- extension/content.js
+++ extension/content.js
@@ -156,7 +156,7 @@ function getPreferences(){
                 if (!contains_coin_label) {
                     coins_as_label = false;
                 }
-                console.log(res)
+                //console.log(res)
                 let prefs_given = false;
                 // var preferences = { "whotracksme": ["Facebook", "Amazon"], "privacyspy": [], "google_safeBrowsing": [], "phishstats": [], "webrisk": [] }
                 for (let i = 0; i < res.length; i++) {
@@ -249,7 +249,7 @@ preferences_promise.then((res)=>{
     xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     //console.log(JSON.stringify(res))
-    console.log(expert);
+    //console.log(expert);
     xhttp.send(JSON.stringify({"urls": urls, "preferences": JSON.stringify(res), "expert": expert}));
 })
 
@@ -439,8 +439,140 @@ function getVisitedUrls(output) {
     });
 }
 
+//############################ PRIVACY SUMMARY ####################################
+window.addEventListener("load", function(event) {
+
+    if (document.getElementById("submitButton")) {
+        document.getElementById("url").addEventListener("keyup", function () {
+            if (event.keyCode === 13) { // 13 is the enter key. TODO: Change input to form, so a submit event can be triggered.
+                document.getElementById("submitButton").click();
+            }
+        })
+        document.getElementById("submitButton").addEventListener("click", function () {
+            // call a method that looks for preferences stored in storage api
+            let url = document.getElementById("url").value;
+
+            let preference = getPreferences();
+            preference.then((res) => {
+                let body = JSON.stringify({
+                    "urls": url,
+                    "preferences": JSON.stringify(res),
+                    "expert": false
+                });
+                sendURL(body);
+            });
+        });
+    }
+
+    function sendURL(body) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                // console.log(this.responseText);
+                let labels_score = [
+                    [chrome.runtime.getURL('images/not_found.png')],
+                    [chrome.runtime.getURL('images/green_icon_32.png')],
+                    [chrome.runtime.getURL('images/yellow_icon_32.png')],
+                    [chrome.runtime.getURL('images/red_icon_32.png')]
+                ];
+                let output = JSON.parse(JSON.parse(this.responseText)); // dont know why but you have to parse it twice
+                let first_key = Object.keys(output)[0];
+                document.getElementById("calcLabel").style.display = "block";
+                let img = labels_score[output[first_key][0]["label"]];
+                console.log(output[first_key][0]["label"]);
+                if (output[first_key][1]["label"] === 0) {
+                    document.getElementById("printLabel").innerHTML = "Sorry, we don't have a label for this url yet.";
+                } else {
+                    document.getElementById("printLabel").innerHTML = "The label for the domain " + first_key + " is <img src= " + img + " > </img>.";
+                }
+            }
+        };
+        xhttp.open("POST", "http://127.0.0.1:5000/sendurls/", true); //Flask projekt muss am laufen sein
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhttp.send(body);
+    }
+
+    chrome.storage.local.get(function (data) {
+        let keys = Object.keys(data); //domains
+        let values = Object.values((data)); //labels
+        let all_labels = 0;
+        let goodLabels = 0; //green or gold
+        let unknownLabels = 0;
+
+        //console.log(keys + ': ' + values); //UNCOMMENT TO SEE WHAT LABELS ARE SAVED
+
+        for (let i = 0; i < values.length - 1; i++) {
+            if (values[i] !== 'pages') {
+                all_labels++;
+            }
+            if (!(3 < values[i])) {
+                if (values[i] === 0) {
+                    unknownLabels++;
+                    continue;
+                }
+                goodLabels++;
+            }
+        }
+        let score = '';
+
+        let privacyScoreNumber = document.getElementById("privacyScore_number");
+        let privacyScoreInfo = document.getElementById("privacyScore_info");
+
+        if (privacyScoreNumber && privacyScoreInfo) {
+            if (all_labels === 0) {
+                privacyScoreNumber.innerHTML = '¯\\_(ツ)_/¯';
+                privacyScoreInfo.innerHTML = 'Nothing to show yet.';
+            } else {
+                score = Math.round(goodLabels / (all_labels - unknownLabels) * 100);
+                privacyScoreNumber.innerHTML = score.toString() + '%';
+                privacyScoreInfo.innerHTML = getPrivacyInfo(score);
+            }
+        }
+
+        if (document.getElementById('all_labels')) {
+            let listOfLabels = '';
+            for (let i = 0; i <= all_labels; i++) {
+                if (keys[i] !== 'pages') {
+                    listOfLabels += keys[i] + ': ' + values[i] + '<br>';
+                }
+            }
+            document.getElementById('all_labels').innerHTML = listOfLabels;
+        }
+        //console.log(goodLabels + "/ (" + all_labels + " - " + unknownLabels + ")"); //check result
+    });
 
 
+    function getPrivacyInfo(score) {
+        let info = "";
+
+        if (score >= 66) {
+            info = "Good job! You took good care about your privacy: " +
+                score + "% of your visited websites had a good label!";
+        } else if (score < 66 && score >= 33) {
+            info = "Not bad! You did quite well: " +
+                score + "% of your visited websites had a good label.";
+        } else {
+            info = "Could be better! Only " + score + "% of your visited websites had a good label. " +
+                "Next time take a look at the label before you open the page."
+        }
+        return info;
+    }
+
+//Reset statistics (Privacy Score)
+    if (document.getElementById("reset_stats")) {
+        document.getElementById("reset_stats").addEventListener("click", function () {
+            chrome.storage.local.get(function (data) {
+                if (Object.values(data).length === 0) {
+                    alert("Your storage is already empty");
+                } else {
+                    chrome.storage.local.clear();
+                    location.reload();
+                }
+            });
+        });
+    }
+});
 /* code dumpster
 -------------------------------------------------------------
 function findLabel(domain, label_index){
